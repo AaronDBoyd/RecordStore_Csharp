8bcc76c75506a31cb3ac1ad97aba19714abef8b1
Aaron Boyd <a.boyd91@yahoo.com>
2022-08-19 03:22:48 -0700

    create Song model

---
+++ RecordStore.Solution/RecordStore/Models/Song.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+
+namespace RecordStore.Models
+{
+  public class Song
+  {
+    public string Title { get; set; }
+    public int Id { get; }
+    private static List<Song> _instances = new List<Song> {};
+
+    public Song(string title)
+    {
+      Title = title;
+      _instances.Add(this);  
+      Id = _instances.Count;
+    }   
+
+    public static List<Song> GetAll() 
+    {
+      return _instances; 
+    }
+
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
+
+    public static Song Find(int searchId)
+    {
+      return _instances[searchId-1];
+    }
+  }
+}
