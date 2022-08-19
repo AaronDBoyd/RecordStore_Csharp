cb389afccfd4b63eac163cd7377f8c5c52ace57a
Aaron Boyd <a.boyd91@yahoo.com>
2022-08-19 03:04:08 -0700

    write and pass test

--- RecordStore.Solution/RecordStore.Tests/ModelTests/AlbumTests.cs
+++ RecordStore.Solution/RecordStore.Tests/ModelTests/AlbumTests.cs
@@ -20,5 +20,19 @@ namespace RecordStore.Tests
       Album newAlbum = new Album("test album");
       Assert.AreEqual(typeof(Album), newAlbum.GetType());
     }
+
+    [TestMethod]
+    public void GetName_ReturnsName_String()
+    {
+      //Arrange
+      string name = "Test Album";
+      Album newAlbum = new Album(name);
+
+      //Act
+      string result = newAlbum.Name;
+
+      //Assert
+      Assert.AreEqual(name, result);
+    }
   }
 }
