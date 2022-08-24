aa3d863021eebf26a4ba11c4c8b5d583dfbfe487
Aaron Boyd <a.boyd91@yahoo.com>
2022-08-24 16:51:20 -0700

    configure project to use entity and database

--- RecordStore.Solution/RecordStore/Startup.cs
+++ RecordStore.Solution/RecordStore/Startup.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using RecordStore.Models;
 
 namespace RecordStore
 {
@@ -21,6 +23,10 @@ namespace RecordStore
     public void ConfigureServices(IServiceCollection services)
     {
       services.AddMvc();
+
+      services.AddEntityFrameworkMySql()
+        .AddDbContext<ToDoListContext>(options => options
+        .UseMySql(Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(Configuration["ConnectionStrings:DefaultConnection"])));
     }
 
     public void Configure(IApplicationBuilder app)
